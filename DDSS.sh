9ddc0634db0b0e123cca055f351813d6e8fcc895
SANCLA <github-replies@sancla.com>
2020-04-20 17:16:02 +0200

    Update DDSS.sh

--- DDSS.sh
+++ DDSS.sh
@@ -6,7 +6,7 @@ DDSPDEBUG=1
 
 echo "##################################################"
 echo "### Domoticz Diagnostic Support Package (DDSP) ###"
-echo "### version: 0.0002                            ###"
+echo "### version: 0.0003                            ###"
 echo "##################################################"
 echo 
 echo ">>> Starting Diagnostic Package..."
